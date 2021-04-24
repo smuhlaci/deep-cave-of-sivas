148e0ee2b45b1f2f3363d4c8b8288d81dc2d5872
Sercan Muhlacı <sercan.muhlaci@mynetgroup.com>
2021-04-24 15:12:22 +0300

    Initial Assets

---
+++ Assets/Scripts/Dialog System/Dialog.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnnamedDialog", menuName = "Create Dialog")]
+public class Dialog : ScriptableObject
+{
+    public string[] message;
+}

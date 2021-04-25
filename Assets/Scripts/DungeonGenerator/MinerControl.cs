eeba461d523b41b2ba6f9f43673fb1a8f5f509ed
UmutGurdal <72893135+UmutGurdal@users.noreply.github.com>
2021-04-25 15:24:16 +0300

    PrefabsFixed

--- Assets/Scripts/DungeonGenerator/MinerControl.cs
+++ Assets/Scripts/DungeonGenerator/MinerControl.cs
@@ -7,11 +7,13 @@ public class MinerControl : MonoBehaviour
     public float playerMoveSpeed;
     void Update()
     {
-        MinerMove();
+        MinerMove(); 
+        Debug.Log("x: " + (int)transform.position.x + ", y: " + (int)transform.position.y);
     }
 
     void MinerMove() 
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal") * playerMoveSpeed * Time.deltaTime, Input.GetAxis("Vertical") * playerMoveSpeed * Time.deltaTime, 0);
+        
     }
 }
